44dfbc525f446eb9df507f6f410be1e7a05b387f
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-23 15:47:49 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// app/layout.tsx
+
+export const metadata = {
+  title: "Artistly - Book Your Performer",
+  description: "A performing artist booking platform"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
